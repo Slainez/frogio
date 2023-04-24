8992efb4659fd0b124aa133d22c3cee42ea7a904
loicordono <124904969+loicordono@users.noreply.github.com>
2023-04-24 11:57:25 +0200

    cc

--- views/Utilisateur.php
+++ views/Utilisateur.php
@@ -6,6 +6,9 @@ $fc = new UtilisateurController();
 $results = $fc->setView();
 
 foreach ($results as $utilisateur) {
+  echo $utilisateur->getId_utilisateur() . '<br>';
   echo $utilisateur->getPseudo() . '<br>';
-  echo $utilisateur->getMail();
+  echo $utilisateur->getMail() . '<br>';
+  echo $utilisateur->getPassword() . '<br>';
+
 }
